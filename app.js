34c7143fdc32c55a62d3cc0a3abaa69d81e011e5
Keith McKenna <keithmckenna@gmail.com>
2021-01-26 22:44:34 -0600

    require post route & start creation of post/blog routing

--- app.js
+++ app.js
@@ -3,15 +3,12 @@ require("dotenv").config();
 const express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
-  port = 3000;
+  port = 3000,
+  postRoute = require("./routes/posts");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-  res.redirect("/blog");
-});
 
 app.listen(port, () => {
   console.log(`*DING!!* Service your app LIVE at localhost:${port}`);
