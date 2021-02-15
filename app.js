719408f3bc86896ace95a3e874ce52f6a60cfd02
Grill3dCheese <keithmckenna@gmail.com>
2021-02-14 18:23:25 -0600

    Mix in packages - flash! for successful and unsuccessful messaging, passport and passport-local for auth, required new User model then required flash and configured passport

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ require("dotenv").config();
 const express = require("express"),
   app = express(),
   mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
   methodOverride = require("method-override"),
   bodyParser = require("body-parser"),
   dayjs = require("dayjs"),
   Blog = require("./models/blog"),
+  User = require("./models/user"),
   port = 3000,
   blogRoute = require("./routes/blogs");
 
@@ -26,8 +30,30 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(flash());
 app.use((req, res, next) => {
   res.locals.dayjs = dayjs;
+  next();
+});
+
+// Passport Configuration
+app.use(
+  require("express-session")({
+    secret: process.env.XTRA_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
   next();
 });
 
