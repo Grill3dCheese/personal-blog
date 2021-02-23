204dcabe059e14189d672c9d268d35f15e58e8b8
Grill3dCheese <keithmckenna@gmail.com>
2021-02-23 17:17:33 -0600

    create edit, update and destroy routing to complete RESTful routing and full CRUD app! - YAY

--- routes/blogs.js
+++ routes/blogs.js
@@ -57,4 +57,45 @@ router.get("/:id", (req, res) => {
   });
 });
 
+// edit post route
+router.get("/:id/edit", (req, res) => {
+  Blog.findById(req.params.id, (err, foundBlog) => {
+    if (err || !foundBlog) {
+      req.flash("error", "Apologies, that entry was not found!");
+      res.redirect("back");
+    } else {
+      res.render("blogs/edit", { blog: foundBlog });
+    }
+  });
+});
+
+// update post route
+router.put("/:id", (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, blog) => {
+    if (err) {
+      req.flash("error", err.message);
+      res.redirect("back");
+    } else {
+      req.flash("success", "This entry has been successfully updated!");
+      res.redirect("/blog/" + blog._id);
+    }
+  });
+});
+
+// destroy post route
+router.delete("/:id", (req, res) => {
+  Blog.findByIdAndRemove(req.params.id, (err) => {
+    if (err) {
+      req.flash(
+        "error",
+        "Hmmm, something went wrong. That entry was not deleted."
+      );
+      res.redirect("/blog");
+    } else {
+      req.flash("success", "BOOM! Success! Entry successfully deleted!");
+      res.redirect("/blog");
+    }
+  });
+});
+
 module.exports = router;
