7765d55b7729160bd45a4fb85c101cce4fd9ef42
Keith McKenna <keithmckenna@gmail.com>
2021-07-31 18:42:08 -0500

    select textarea and start CMS toolbar to assist with new blog submissions and formatting

--- public/scripts/customScripts.js
+++ public/scripts/customScripts.js
@@ -1,7 +1,15 @@
 const alertMsg = document.querySelector(".alert-container");
+const textArea = document.querySelector("textarea");
 
 if (alertMsg) {
   window.setTimeout(function () {
     alertMsg.classList.add("slide-up");
   }, 3000);
 }
+
+// New Blog CMS Toolbar v.1.0.0
+
+const breakingSpace = () => {
+  textArea.value += "\n<br>";
+  textArea.focus();
+};
