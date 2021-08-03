939d044565e7c67ff3237d214d92369c46f331cf
Keith McKenna <keithmckenna@gmail.com>
2021-08-02 19:21:06 -0500

    create functions that will serve new blog CMS toolbar to help easily input html tags into textarea while writing new blogs

--- public/scripts/customScripts.js
+++ public/scripts/customScripts.js
@@ -13,3 +13,28 @@ const breakingSpace = () => {
   textArea.value += "\n<br>";
   textArea.focus();
 };
+
+const paragraph = () => {
+  textArea.value += "<p></p>";
+  textArea.focus();
+};
+
+const emphasized = () => {
+  textArea.value += "<em></em>";
+  textArea.focus();
+};
+
+const strong = () => {
+  textArea.value += "<strong></strong>";
+  textArea.focus();
+};
+
+const superText = () => {
+  textArea.value += "<super></super>";
+  textArea.focus();
+};
+
+const horizontalRule = () => {
+  textArea.value += "<hr>";
+  textArea.focus();
+};
